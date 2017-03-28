d5ca591c4d52d77dc3e2860aa141634e2c3fc9f4
Ujjwal Arora <arora.ujjwal@gmail.com>
2017-03-29 03:01:41 +0530

    Build inital model

--- Prediction.R
+++ Prediction.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(caTools)
 library(randomForest)
+library(glmnet)
+library(plyr)
 
 train <- read.csv("Documents/GitHub/Big-Mart-Sales-Prediction/Train_UWu5bXk.csv")
 test <- read.csv("Documents/GitHub/Big-Mart-Sales-Prediction/Test_u94Q5KV.csv")
@@ -65,4 +67,51 @@ nRows <- nrow(train)
 nCompRows <- sum(complete.cases(train))
 nCompRows/nRows
 
+# Get final summary
+summary(train)
 
+# Do same everything for test
+test$Item_Fat_Content <- gsub("LF", "lowfat",test$Item_Fat_Content)
+test$Item_Fat_Content <- gsub("low fat", "lowfat",test$Item_Fat_Content)
+test$Item_Fat_Content <- gsub("Low Fat", "lowfat",test$Item_Fat_Content)
+test$Item_Fat_Content <- gsub("reg", "Regular",test$Item_Fat_Content)
+test$Item_Fat_Content <- as.factor(test$Item_Fat_Content)
+MeanItem_Weight <- mean(test$Item_Weight[!is.na(test$Item_Weight)])
+test$Item_Weight[is.na(test$Item_Weight)] <- MeanItem_Weight
+test_temp <- test %>% filter(Item_Visibility != 0)
+visibility_model <- lm(Item_Visibility ~ Item_Weight + Item_Fat_Content +
+                         Item_Type + Item_MRP +
+                         Outlet_Establishment_Year + Outlet_Size +
+                         Outlet_Location_Type,
+                       data = test_temp)
+test$Item_Visibility[test$Item_Visibility == 0] <-
+  predict(visibility_model,newdata = test[test$Item_Visibility == 0,])
+set.seed(100)
+test$Outlet_Size <- as.character(test$Outlet_Size)
+Storetypes <- subset(test, Outlet_Size != "")
+spl <- sample.split(Storetypes$Outlet_Size, SplitRatio = 0.8)
+test_outlet <- subset(Storetypes, spl == TRUE)
+test_outlet <- subset(Storetypes, spl == FALSE)
+test_outlet$Outlet_Size <- as.factor(test_outlet$Outlet_Size)
+test_outlet$Outlet_Size <- as.factor(test_outlet$Outlet_Size)
+SizeForest <- randomForest(Outlet_Size ~.-Item_Identifier,
+                           data =  test_outlet,nodesize = 25, ntree = 100)  
+PredictForest <- predict(SizeForest, newdata = test_outlet)
+test$Outlet_Size <- predict(SizeForest, newdata = test)
+
+
+# Build the model
+test_pred <- test
+sales_model <- lm(Item_Outlet_Sales ~ Item_Weight + Item_Fat_Content +
+                         Item_Visibility + Item_MRP + 
+                         Outlet_Establishment_Year + Outlet_Size +
+                         Outlet_Location_Type + Outlet_Type,
+                       data = train)
+sales <- predict(sales_model, newdata = test_pred)
+test_pred$Item_Outlet_Sales <- as.vector(sales)
+
+# Rename the predicted sales column
+names(test_pred)[12] <- "Item_Outlet_Sales"
+
+answer = test_pred[, c("Item_Identifier", "Outlet_Identifier", "Item_Outlet_Sales")]
+write.csv(answer, "Documents/GitHub/Big-Mart-Sales-Prediction/Result.csv")
